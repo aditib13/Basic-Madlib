e5f42e103a534bc86b53ed8a57bc53864f7cbbf1
Aditi Bhandari <aditibhandari24@gmail.com>
2014-08-13 17:30:24 -0400

    Create madlib.py
    Basic Madlib - Takes input and fills in blanks

---
+++ madlib.py
@@ -0,0 +1,15 @@
+
+place = raw_input("Place: ")
+noun = raw_input("Noun: ")
+verb = raw_input("Verb: ")
+bodypart = raw_input("Body Part: ")
+emotion = raw_input("Emotion: ")
+color = raw_input("Color: ")
+size = raw_input("Size: ")
+superhero = raw_input("Superhero: ")
+
+print "You walk into a %r and see %r. You %r to the %r 
+\nand punch it. Suddenly, %r stands up and punches you in the 
+\n%r. You get %r and suddenly your skin turns %r. You grow %r 
+\nand realize you are %r. You kill %r and tell them not to 
+\nmess with you again." % (place, noun, verb, noun, noun, bodypart, emotion, color, size, superhero, noun)
